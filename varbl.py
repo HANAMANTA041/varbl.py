afdfda483e2c8270f0f36403ec97c41822f2457b
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-16 10:27:33 +0530

    varbl.md

---
+++ varbl.py
@@ -0,0 +1,55 @@
+"""
+x=5    #we don't need to declare any data types for the variables its by defualt
+y="John" # variable declaration
+x="hbs"
+y='hbs'# "x" and 'x' both of them are strings
+print(x)
+print(y)
+print(x)
+print(y)
+x=10
+x="John" #the last x value is printed and also changes ite data type
+print(x)
+# casting which checks the data type
+x=str(5)
+y=int(5)
+z=float(5)
+print(x, y, z)
+print(type(x))#displays data types
+print(type(y))
+print(type(z))
+a=5
+A=6
+#variables are basically case sensitive
+print(a)
+print(A)
+#variable names a,b,c or age,car,total_volume etc
+#it should start with a letter or the underscore and cannot start with a number
+# only contains alphanumeric a to z or A to Z or _ not special characters
+#case sensitive
+#Assign multiple values to variables
+a12 = 1 #12a = 1 syntaxerror
+print(a12)
+a= "Apple"
+print(a)
+b= "Ball"
+print(b)
+c= "cat"
+print(c)
+#or
+x,y,z="Banaan","Cherry","Orange"
+print(x)
+print(y)
+print(z)"""
+#Assign multiple variables to one value
+x=y=z= "Oranges"
+
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
